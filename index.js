861ab301d45737eacdb626904ab434d0f9e663ba
Adem Saglam <asaglam84@hotmail.com>
2017-09-06 11:42:25 +0300

    first

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const fs = require('fs');
+const https = require('https');
+
+const app = express();
+
+app.get('/',function(req,res) {
+    res.send({ok: true});
+});
+
+https.createServer({
+    cert: fs.readFileSync('../sslcert/fullchain.pem'),
+    key: fs.readFileSync('../sslcert/key.pem')
+},app).listen(443);
